715f63fc86f3bd2cea60322f93e4bd0ef0f038f2
Başak Sökmen <basaksokmeen@gmail.com>
2022-03-10 11:12:50 +0300

    Proje dosyası ekle.

---
+++ SayiIsaret/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SayiIsaret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 11, -73, 0, 6, -8, 22, 51, 7, -40, 9, 3, -11 };
+            foreach (var sayi in sayilar)
+            {
+                if (sayi > 0 && sayi % 2 == 0)
+                {
+                    Console.WriteLine("{0} sayısı pozitif ve çifttir.", sayi);
+                }
+                if (sayi > 0 && sayi % 2 != 0)
+                {
+                    Console.WriteLine("{0} sayısı pozitif ve tektir.", sayi);
+                }
+                else if (sayi < 0 && sayi % 2 != 0)
+                {
+                    Console.WriteLine("{0} sayısı negatif ve tektir.", sayi);
+                }
+                else if (sayi < 0 && sayi % 2 == 0)
+                {
+                    Console.WriteLine("{0} sayısı negatif ve çifttir.", sayi);
+                }
+                else if (sayi == 0)
+                    Console.WriteLine("{0} sayısı nötr ve işaretsizdir.", sayi);
+            }
+        }
+    }
+}
